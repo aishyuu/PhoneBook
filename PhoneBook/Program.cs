c133566fff2b6e3bd1f403acdbebe8cd85485f4a
aishyuu <ivanmatias1331@gmail.com>
2024-06-26 23:11:20 -0700

    Created main menu prompt.

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+var programRunning = true;
+
+while (programRunning)
+{
+    var mainMenuResponse = AnsiConsole.Prompt(
+        new SelectionPrompt<MenuOptions>()
+        .Title("Select an Action")
+        .AddChoices(
+            MenuOptions.AddContact,
+            MenuOptions.DeleteContact,
+            MenuOptions.UpdateContact,
+            MenuOptions.ViewAllContacts,
+            MenuOptions.ViewContact));
+}
+
+enum MenuOptions
+{
+    AddContact,
+    DeleteContact,
+    UpdateContact,
+    ViewAllContacts,
+    ViewContact
+}
