93112be6a17029d9017f7f2d0ba2833f782a22a4
aishyuu <ivanmatias1331@gmail.com>
2024-06-26 22:18:40 -0700

    Completed DbContext and initial migration.

---
+++ PhoneBook/ContactContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBook;
+
+internal class ContactContext: DbContext
+{
+    public DbSet<Contact> Contacts { get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=PhoneBookDB;Integrated Security = true;TrustServerCertificate=True");
+    }
+}
